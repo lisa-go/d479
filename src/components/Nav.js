899977ad0947a4438a080d6290c4c27f4bfe0150
lisa-go <lisadev0123@gmail.com>
2025-09-12 11:52:30 -0500

    finished designing home page

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,3 +1,19 @@
+import '../styles/nav.css';
+
 export default function Nav({ currentPage, setCurrentPage }) {
-  return <div>Nav</div>;
+  const navList = ['PLAY', 'EAT', 'STAY', 'MOVE'];
+  const tempList = navList.filter((page) => page !== currentPage);
+
+  return (
+    <ul id={currentPage == 'HOME' ? 'home-nav' : undefined}>
+      {tempList.map((n, i) => (
+        <li
+          key={i}
+          id={`${n.toLowerCase()}-nav`}
+          onClick={() => setCurrentPage(n)}>
+          {n}
+        </li>
+      ))}
+    </ul>
+  );
 }
