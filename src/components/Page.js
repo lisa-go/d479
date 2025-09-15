b66cd115c1f67ba0c8c2d88f635f12a2984cc9ba
lisa-go <lisadev0123@gmail.com>
2025-09-15 16:04:36 -0500

    added html and css for filter section

--- src/components/Page.js
+++ src/components/Page.js
@@ -12,6 +12,14 @@ export default function Page({ currentPage, setCurrentPage }) {
     data = activities;
   }
 
+  let category;
+  if (data.length >= 1) {
+    const tempArray = data.map((d) => {
+      return d.category;
+    });
+    category = Array.from(new Set(tempArray));
+  }
+
   return (
     <div id={currentPage.toLowerCase()}>
       <header>
@@ -31,8 +39,60 @@ export default function Page({ currentPage, setCurrentPage }) {
             />
           ))}
         </div>
-        <div className='filters'>
-          <p>filters</p>
+        <div className={`filters ${currentPage.toLowerCase()}-filters`}>
+          <input
+            type='text'
+            name='search'
+            id='search'
+            placeholder='Search'
+          />
+          <div id='filter-options-container'>
+            <h5>Filter options</h5>
+            <div id='budget-container'>
+              <h6>Budget</h6>
+              <div className='option'>
+                <input
+                  type='checkbox'
+                  name='$'
+                  id='$'
+                />
+                <p>$</p>
+              </div>
+              <div className='option'>
+                <input
+                  type='checkbox'
+                  name='$$'
+                  id='$$'
+                />
+                <p>$$</p>
+              </div>
+              <div className='option'>
+                <input
+                  type='checkbox'
+                  name='$$$'
+                  id='$$$'
+                />
+                <p>$$$</p>
+              </div>
+            </div>
+
+            <div id='category-container'>
+              <h6>Category</h6>
+              {category.map((c) => (
+                <div
+                  className='option'
+                  key={c}>
+                  <input
+                    type='checkbox'
+                    name={c}
+                    id={c}
+                  />
+                  <p>{c}</p>
+                </div>
+              ))}
+            </div>
+          </div>
+          <button>Filter</button>
         </div>
       </div>
     </div>
